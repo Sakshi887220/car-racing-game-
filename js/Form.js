2b9e01bdab8ad1355a276ce424574b87ef3ddc9b
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-04-14 16:13:10 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,29 +1,18 @@
 class Form {
   constructor() {
-    this.input = createInput("").attribute("placeholder", "Enter your name");
-    this.playButton = createButton("Play");
-    this.titleImg = createImg("./assets/title.png", "game title");
-    this.greeting = createElement("h2");
+    
   }
 
   setElementsPosition() {
-    this.titleImg.position(120,50);
-    this.input.position(width / 2 - 110, height / 2 - 80);
-    this.playButton.position(width / 2 - 90, height / 2 - 20);
-    this.greeting.position(width / 2 - 300, height / 2 - 100);
+   
   }
 
   setElementsStyle() {
-    this.titleImg.class("gameTitle");
-    this.input.class("customInput");
-    this.playButton.class("customButton");
-    this.greeting.class("greeting");
+    
   }
 
   hide() {
-    this.greeting.hide();
-    this.playButton.hide();
-    this.input.hide();
+   
   }
 
   handleMousePressed() {
@@ -38,8 +27,6 @@ class Form {
   }
 
   display() {
-    this.setElementsPosition();
-    this.setElementsStyle();
-    this.handleMousePressed();
+    
   }
 }
